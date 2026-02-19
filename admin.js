fd20977f4946c4df231e20befe2045519defc08b
MHBEATZ <mhgothit@gmail.com>
2026-02-19 01:55:11 +0000

    Delete admin.js

--- admin.js
+++
@@ -1,8 +0,0 @@
-function generate(){
- output.innerText=JSON.stringify({
-  name:name.value,
-  cover:"assets/artwork/YOUR-ART.jpg",
-  audio:"assets/beats/YOUR-BEAT.mp3",
-  licenses:{MP3:+mp3.value,WAV:+wav.value,UNLIMITED:+unl.value}
- },null,2);
-}
